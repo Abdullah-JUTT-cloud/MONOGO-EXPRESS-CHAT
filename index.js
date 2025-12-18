83e08de44d130d06c0e2d26008c06a1cab6cf0ef
Abdullah <itxabdullah44@gmail.com>
2025-12-19 00:02:09 +0500

    basic setup done

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express=require('express');
 const app=express();
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+const path=require("path");
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+main().then((res)=>{
+    console.log("connection succes!");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/WETOSH');
+}
+
 app.get("/",(req,res)=>{
     res.send("root is working");
 });
