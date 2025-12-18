79e9c9b77eaf67b15960081e11b5afa91d82ad69
Abdullah <itxabdullah44@gmail.com>
2025-12-19 01:01:41 +0500

    some styling and templating

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ mongoose.set('strictQuery', true);
 const path=require("path");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
 
 const Chat=require("./models/chat.js");
 
@@ -29,6 +30,11 @@ async function main() {
 //   console.log("All books deleted");
 // }
 
+app.get("/chats",async(req,res)=>{
+    let chats=await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
 
 app.get("/",(req,res)=>{
     res.send("root is working");
