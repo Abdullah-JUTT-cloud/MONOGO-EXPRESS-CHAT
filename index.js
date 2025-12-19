dd5ced63910aa4aba3139939657c575cc2aaebc9
Abdullah <itxabdullah44@gmail.com>
2025-12-19 17:57:32 +0500

    post route

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express=require('express');
 const app=express();
-app.use(express.urlencoded({extended:true}));
-
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-
+const Chat=require("./models/chat.js");
 const path=require("path");
+const methodOverride=require("method-override");
+
 app.set("views",path.join(__dirname,"views"));
+mongoose.set('strictQuery', true);
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
-
-const Chat=require("./models/chat.js");
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 
 
@@ -33,7 +33,7 @@ async function main() {
 
 app.get("/chats",async(req,res)=>{
     let chats=await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
 });
 
@@ -54,6 +54,20 @@ app.post("/chats",(req,res)=>{
    }).catch((err)=>{
     console.log(err);
    });
+    res.redirect("/chats");
+});
+
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let chat= await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
+app.put("/chats/:id",async(req,res)=>{
+    let {id}=req.params;
+    let {msg:newMsg}=req.body;
+    let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
+    console.log(updatedChat);
     res.redirect("/chats");
 });
 
